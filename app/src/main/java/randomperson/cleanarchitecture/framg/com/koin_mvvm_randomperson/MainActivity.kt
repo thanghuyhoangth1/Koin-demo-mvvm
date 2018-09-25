8032c36c4c14ad622ae22f54e08165806358a821
thang.huy.hoang <FRAMGIA\thang.huy.hoang@framgia0384-pc.framgia.com>
2018-09-25 14:38:45 +0700

    init project

---
+++ app/src/main/java/randomperson/cleanarchitecture/framg/com/koin_mvvm_randomperson/MainActivity.kt
@@ -0,0 +1,12 @@
+package randomperson.cleanarchitecture.framg.com.koin_mvvm_randomperson
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
